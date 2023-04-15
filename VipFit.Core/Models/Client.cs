0b66d8bc3aa25e9215cdea18e1e334143bddaed5
Jakub Gramburg <jacubgr@gmail.com>
2023-04-15 19:09:25 +0200

    add base client model

---
+++ VipFit.Core/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿namespace VipFit.Core.Models
+{
+    /// <summary>
+    /// Represents a client entity.
+    /// </summary>
+    internal class Client : DbObject, IEquatable<Client>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        public bool Equals(Client other) =>
+            other != null &&
+            FirstName == other.FirstName &&
+            LastName == other.LastName &&
+            Phone == other.Phone &&
+            Email == other.Email;
+    }
+}
---
+++ VipFit.Core/Models/DbObject.cs
@@ -0,0 +1,13 @@
+﻿namespace VipFit.Core.Models
+{
+    /// <summary>
+    /// Base class for database entities.
+    /// </summary>
+    internal class DbObject
+    {
+        /// <summary>
+        /// Gets or sets the database id.
+        /// </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
