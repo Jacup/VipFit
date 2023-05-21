7660e69e2e99e141d0298fc47924e891c9eeee1c
Jakub Gramburg <jacubgr@gmail.com>
2023-05-21 21:32:25 +0200

    Implement Payment Model and fix relationships between pass and dependends

---
+++ VipFit.Core/Models/Payment.cs
@@ -0,0 +1,36 @@
+﻿namespace VipFit.Core.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Payment : DbObject
+    {
+        [Required]
+        public double Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly DueDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? PaymentDate { get; set; }
+
+        [Required]
+        public bool Paid { get; set; }
+
+        [DataType(DataType.Text)]
+        [MaxLength(100)]
+        public string Comment { get; set; }
+
+        #region Foreign Keys
+
+        [Required]
+        public Guid PassId { get; set; }
+
+        [ForeignKey(nameof(PassId))]
+        public Pass Pass { get; set; }
+
+        #endregion
+    }
+}
