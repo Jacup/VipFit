866de7cc9107f797b3d9c069e10b1528aea85654
Jakub Gramburg <jacubgr@gmail.com>
2023-05-28 21:40:02 +0200

    Update DbObject comments

--- VipFit.Core/Models/DbObject.cs
+++ VipFit.Core/Models/DbObject.cs
@@ -1,14 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace VipFit.Core.Models
+﻿namespace VipFit.Core.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
     /// Base class for database entities.
     /// </summary>
     public class DbObject
     {
         /// <summary>
-        /// Gets or sets the database id.
+        /// Gets or sets the entity id.
         /// </summary>
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
